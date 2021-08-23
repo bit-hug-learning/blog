6b488d39ed6e0349721a2510f60d1ca68c67e8e8
Gonzo <gvidal.industrial@gmail.com>
2021-08-23 18:53:58 -0500

    Add dark and size theme

--- styles/globalStyles.js
+++ styles/globalStyles.js
@@ -3,8 +3,6 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   html,
   body {
-    padding: 0;
-    margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
   }
@@ -15,8 +13,12 @@ const GlobalStyle = createGlobalStyle`
   }
 
   * {
+    padding: 0;
+    margin: 0;
     box-sizing: border-box;
+    font-size: 62.5%
   }
+
 `;
 
 export default GlobalStyle;
--- styles/theme.js
+++ styles/theme.js
@@ -5,6 +5,26 @@ const theme = {
     md: '768px',
     lg: '1280px',
   },
+  dark: {
+    primary: '#226698',
+    secondary: '#292929',
+    postPrimary: '#505050',
+    postSecondary: '#4a6b83',
+    borderPrimary: '#379EFE',
+    borderSecondary: '#8000FF',
+    buttonText: '#000000',
+    buttonBG: '#78AEFF',
+    buttonHoverBG: '#B1D0FF',
+    text: '#FFFFFF',
+    textSecondary: '#817F7F',
+    logosHeader: '#979797',
+  },
+  size: {
+    s: '1.2rem',
+    m: '1.6rem',
+    l: '2.4rem',
+    xl: '3rem',
+  },
 };
 
 export default theme;
